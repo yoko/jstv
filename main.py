4aaacfd4649a371124bce1ed248a111ed48af0a0
yksk <yoko@carbonfairy.org>
2010-12-28 21:00:17 +0900

    :)

---
+++ main.py
@@ -0,0 +1,36 @@
+#!python
+
+import os
+import os.path
+import logging
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class QUnitHandler(webapp.RequestHandler):
+  def get(self):
+    base = self.request.get('base')
+    scripts = self.request.get('script', allow_multiple=True)
+    title = self.request.get('title') or scripts and os.path.basename(scripts[0]) or 'QUnit'
+
+    path = os.path.join(os.path.dirname(__file__), 'templates/qunit.html')
+    t = {
+      'title'  : title,
+      'base'   : base,
+      'scripts': scripts,
+    }
+    self.response.out.write(template.render(path, t))
+
+
+def main():
+    application = webapp.WSGIApplication(
+      [
+        ('/qunit', QUnitHandler)],
+      debug=True)
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
